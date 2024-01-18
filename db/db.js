20428631325833eba4c1497eee240b5a4976ab5f
Toro <aatn1321@gmail.com>
2024-01-18 20:32:45 -0300

    adsadf

--- db/db.js
+++ db/db.js
@@ -2,10 +2,15 @@
 const mongoose = require("mongoose");
 
 
+const username = encodeURIComponent(process.env["USER_DB"]);
+const password = encodeURIComponent(process.env["USER_PASSWORD"]);
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.p7vsale.mongodb.net/?retryWrites=true&w=majority`
+
 const conectarBaseDeDatos = async () => {
     try {
 
-        await mongoose.connect(`mongodb+srv://aatn1321:oQTU17OII4WMwb1D@cluster0.p7vsale.mongodb.net/?retryWrites=true&w=majority`, {
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
